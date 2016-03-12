3efb57700aa032479a110e0f1510967930d7c01e
Fayimora Femi-Balogun <fayi@fayimora.com>
2016-03-12 07:42:47 +0000

    Add Toolbar and make it hide/show when scrolling :wink:

--- app/src/main/java/com/fayimora/mycontacts/ContactListActivity.java
+++ app/src/main/java/com/fayimora/mycontacts/ContactListActivity.java
@@ -2,8 +2,10 @@ package com.fayimora.mycontacts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -16,15 +18,36 @@ public class ContactListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_list);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         ArrayList<Contact> contacts = new ArrayList<>();
 
-        Contact c1 = new Contact();
-        c1.setName("Fayimora");
-        contacts.add(c1);
+        for (int i=0; i<30; i++){
+            Contact c1 = new Contact();
+            c1.setName("Fayimora");
+            contacts.add(c1);
+        }
 
         ListView listView = (ListView) findViewById(R.id.contact_list_view);
         listView.setAdapter(new ContactAdapter(contacts));
+        listView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            int previousFirstIem = 0;
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                if(firstVisibleItem > previousFirstIem){
+                    getSupportActionBar().hide();
+                } else if(firstVisibleItem < previousFirstIem) {
+                    getSupportActionBar().show();
+                }
+                previousFirstIem = firstVisibleItem;
+            }
+        });
     }
 
     private class ContactAdapter extends ArrayAdapter<Contact> {
